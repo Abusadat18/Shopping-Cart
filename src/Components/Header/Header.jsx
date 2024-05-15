b1d79680253edcfaf96dbebd54ea117c75687dba
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-05-15 15:37:08 +0530

    Create Header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import styles from "./Header.module.css"
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className={styles.header}>
+      <div className={styles.top}>
+        <h1>BAZAAR PLAZA</h1>
+        <div className={styles.actions}>
+          <Link><img src="/account-circle.svg" alt="" width={35} /></Link>
+          <NavLink to="myCart"><img src="/cart.svg" alt="" width={35} /></NavLink>
+        </div>
+      </div>
+      <nav>
+        <NavLink to="/" className={styles.navLink}>Home</NavLink>
+        <NavLink to="shop" className={styles.navLink}>Shop</NavLink>
+      </nav>
+    </header>
   )
 }
 
