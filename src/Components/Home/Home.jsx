a79c275cb09e89a681a8064382771438f9aa8c27
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-05-15 16:26:00 +0530

    Add Home Component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from "./Home.module.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <section className={styles.home}>
+      <img className={styles.homeImg} src="/home-logo.png" alt="" />
+      <p className={styles.about}>Welcome to Bazaar Plaza, your premier online shopping destination! Discover a wide range of products, from traditional crafts to modern electronics. Explore our spice market for delightful aromas, indulge in global cuisines at our eateries, and experience live events. Shop now for fantastic deals and an exceptional shopping journey at Bazaar Plaza!</p>
+    </section>
   )
 }
 
