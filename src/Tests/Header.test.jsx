4bd46e3d29434fdd63585f1b654504d2f7dbbe54
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-05-26 11:30:18 +0530

    Add test for Header

--- src/Tests/Header.test.jsx
+++ src/Tests/Header.test.jsx
@@ -1,4 +1,15 @@
-import { describe } from "vitest";
+import { describe, expect, test } from "vitest";
 import Header from "../Components/Header/Header";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 
-describe("Header Component" => {})
+describe("Header Component", () => {
+    test("render header", () => {
+        const { container } = render(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>
+        );
+        expect(container).toMatchSnapshot();
+    });
+});
